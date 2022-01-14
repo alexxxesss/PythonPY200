8c4791f3278407086e71eba7b80f785ab913c5fb
alexxxesss <alexeenkoalexxx@yandex.ru>
2022-01-15 01:54:02 +0300

    1 practice

--- Основы ООП/Лабораторные задания/task4_Node_list_comprehension_loop_for/main.py
+++ Основы ООП/Лабораторные задания/task4_Node_list_comprehension_loop_for/main.py
@@ -4,14 +4,14 @@ from typing import Any, Optional
 class Node:
     """ Класс, который описывает узел связного списка. """
 
-    def __init__(self, value: Any, next_: Optional["Node"] = None):
+    def __init__(self, value: Any, next: Optional["Node"] = None):
         """
         Создаем новый узел для односвязного списка
         :param value: Любое значение, которое помещено в узел
         :param next_: следующий узел, если он есть
         """
         self.value = value
-        self.next = next_
+        self.next = next
 
     def __repr__(self) -> str:
         return f"Node({self.value}, {self.next})"
