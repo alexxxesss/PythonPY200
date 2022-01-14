1896b5c067afbf83d40b9884be7d77b869ddf9a4
Alex-PC-home <alexeenkoalexxx@yandex.ru>
2022-01-15 00:50:09 +0300

    1 practice

--- Основы ООП/Лабораторные задания/task7_Glass/task.py
+++ Основы ООП/Лабораторные задания/task7_Glass/task.py
@@ -1,7 +1,39 @@
 from typing import Union
 
 
-# TODO  создать класс Glass
+class Glass:
+    def __init__(self, capacity_volume: Union[int, float], occupied_volume: Union[int, float]):
+        self.capacity_volume = None
+        self.__init_capacity_volume(capacity_volume)
+
+        self.occupied_volume = None
+        self.__init_occupied_volume(occupied_volume)
+
+    def __init_capacity_volume(self, value: Union[int, float]):
+        """
+        Функция проверяет задаваемый объем стакана
+
+        :param value: значение объема стакана
+        :return: значение задаваемого объема стакана после проверки типов
+        """
+        if not isinstance(value, (int, float)):
+            raise TypeError
+        if not value > 0:
+            raise ValueError
+        self.capacity_volume = value
+
+    def __init_occupied_volume(self, value: Union[int, float]):
+        """
+        Функция проверяет занятый объем стакана
+
+        :param value: значение занятого объема стакана
+        :return: значение занятого объема стакана после проверки типов
+        """
+        if not isinstance(value, (int, float)):
+            raise TypeError
+        if value < 0:
+            raise ValueError
+        self.occupied_volume = value
 
 
 if __name__ == "__main__":
