3ad55a64143a129ed83a435ad42f8cf9942080bc
alexxxesss <alexeenkoalexxx@yandex.ru>
2022-01-18 23:17:59 +0300

    3 practice

--- Инкапсуляция, наследование, полиморфизм/Car/Class_car.py
+++ Инкапсуляция, наследование, полиморфизм/Car/Class_car.py
@@ -1,7 +1,17 @@
+import time
+
 from driver import Driver
 
 
 class DriverTypeError(Exception):
+    pass
+
+
+class EngineIsNotRunning(Exception):
+    pass
+
+
+class DriverNotFoundError(Exception):
     pass
 
 
@@ -19,7 +29,9 @@ class Car:
         self.complectation = complectation
         self.color = color
 
+        self.__mileage = 0
         self.__driver = None
+        self.__engine_status = True
 
     @property
     def driver(self):
@@ -42,12 +54,51 @@ class Car:
     # def get_driver(self):
     #     return self.driver
 
+    def start_engine(self):
+        self.__engine_status = True
+
+    def __check_driver(self):
+        if self.driver is not None:
+            return True
+        return False
+
+    def __ready_status(self):
+        if not self.__engine_status:
+            raise EngineIsNotRunning("двигатель не запущен")
+        if not self.__check_driver():
+            raise DriverNotFoundError("водитель не найден")
+
+        # if self.__engine_status and self.__check_driver():
+        #     return True
+        # return False
+
+    def move(self, distance=10):
+        try:
+            if self.__ready_status():
+                for i in range(distance):
+                    print(f"Машина проехала {i+1} км")
+                    time.sleep(0.3)
+                print("Путь пройден")
+        except (EngineIsNotRunning, DriverNotFoundError) as e:
+            print(f'Машина не может начать движение, т.к. {e}')
+
+    @property
+    def _mileage(self):
+        repr(self.__mileage)
+
+    @_mileage.setter
+    def _mileage(self):
+        if not isinstance(mileage, (int, float)):
+            raise T
+
 
 if __name__ == "__main__":
 
     car = Car("Черный", "седан", "модель", "бензин", "автомат", "люкс")
+    car.start_engine()
+
     car.driver = Driver("Иван")
-    print(car.driver)
+    car.move()
 
 # class Nissan(Car):
 #     brand = "Nissan"
