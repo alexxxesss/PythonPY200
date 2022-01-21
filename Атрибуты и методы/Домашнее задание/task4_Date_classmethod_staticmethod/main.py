c15f95530a15ca706219a179f46ca8f06b2894b7
Alex-PC-home <alexeenkoalexxx@yandex.ru>
2022-01-21 22:19:47 +0300

    2 practice

--- Атрибуты и методы/Домашнее задание/task4_Date_classmethod_staticmethod/main.py
+++ Атрибуты и методы/Домашнее задание/task4_Date_classmethod_staticmethod/main.py
@@ -14,19 +14,40 @@ class Date:
 
         self.is_valid_date(self.day, self.month, self.year)
 
-    def is_leap_year(self, year: int):
+    @staticmethod
+    def __is_leap_year(year: int):
         """Проверяет, является ли год високосным"""
-        ...  # TODO
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        elif year % 4 == 0:
+            return True
+        else:
+            return False
 
-    def get_max_day(self, month: int, year: int):
+    def __get_max_day(self, month: int, year: int):
         """Возвращает максимальное количество дней в месяце для указанного года"""
-        ...  # TODO
+        if 12 < month <= 0:
+            raise ValueError("Неверно введен месяц")
+
+        if self.__is_leap_year(year):
+            day_on_month = self.DAY_OF_MONTH[1][month-1]
+        else:
+            day_on_month = self.DAY_OF_MONTH[0][month-1]
+
+        return day_on_month
 
     def is_valid_date(self, day: int, month: int, year: int):
         """Проверяет, является ли дата корректной"""
-        ...  # TODO
+        day_on_month = self.__get_max_day(month, year)
+        if day <= 0 or day > day_on_month:
+            raise ValueError("Неверно введен день")
 
+        print("Дата введена корректно")
 
 if __name__ == "__main__":
-    # Write your solution here
-    pass
+    new_year = Date(29, 2, 2022)
+
+
