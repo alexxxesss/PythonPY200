f124886ca7413c4a4b7948fbbf0ae2b328f3b310
Alex-PC-home <alexeenkoalexxx@yandex.ru>
2022-02-02 23:59:37 +0300

    zachet

--- Зачетное задание/Зачетное задание/DoubleLinkedList/task.py
+++ Зачетное задание/Зачетное задание/DoubleLinkedList/task.py
@@ -15,48 +15,6 @@ class LinkedList(MutableSequence):
             for value in data:
                 self.append(value)
 
-    def append(self, value: Any):
-        """ Добавление элемента в конец связного списка. """
-        append_node = self.ABSTRACT_CLASS_NODE(value)
-
-        if self.head is None:
-            self._head = append_node
-        else:
-            last_index = self._len - 1
-            last_node = self.step_by_step_on_nodes(last_index)
-
-            self.linked_nodes(last_node, append_node)
-
-        self._len += 1
-
-    def __len__(self):
-        return self._len
-
-    @staticmethod
-    def linked_nodes(left_node: Node, right_node: Optional[Node] = None) -> None:
-        """
-        Функция, которая связывает между собой два узла.
-
-        :param left_node: Левый или предыдущий узел
-        :param right_node: Правый или следующий узел
-        """
-        left_node.next(right_node)
-
-    def step_by_step_on_nodes(self, index: int) -> Node:
-        """ Функция выполняет перемещение по узлам до указанного индекса. И возвращает узел. """
-
-        if not isinstance(index, int):
-            raise TypeError()
-
-        if not 0 <= index < self._len:  # для for
-            raise IndexError()
-
-        current_node = self.head
-        for _ in range(index):
-            current_node = current_node.next
-
-        return current_node
-
     def __getitem__(self, index: int) -> Any:
         """ Метод возвращает значение узла по указанному индексу. """
         node = self.step_by_step_on_nodes(index)
@@ -88,14 +46,56 @@ class LinkedList(MutableSequence):
 
         self._len -= 1
 
-    def to_list(self) -> list:
-        return [linked_list_value for linked_list_value in self]
+    def __len__(self):
+        return self._len
+
+    def __str__(self) -> str:
+        return f"{self.to_list()}"
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}({self.to_list()})"
 
-    def __str__(self) -> str:
-        return f"{self.to_list()}"
+    @staticmethod
+    def linked_nodes(left_node: Node, right_node: Optional[Node] = None) -> None:
+        """
+        Функция, которая связывает между собой два узла.
+
+        :param left_node: Левый или предыдущий узел
+        :param right_node: Правый или следующий узел
+        """
+        left_node.next(right_node)
+
+    def to_list(self) -> list:
+        return [linked_list_value for linked_list_value in self]
+
+    def append(self, value: Any):
+        """ Добавление элемента в конец связного списка. """
+        append_node = self.ABSTRACT_CLASS_NODE(value)
+
+        if self.head is None:
+            self._head = append_node
+        else:
+            last_index = self._len - 1
+            last_node = self.step_by_step_on_nodes(last_index)
+
+            self.linked_nodes(last_node, append_node)
+
+        self._len += 1
+
+    def step_by_step_on_nodes(self, index: int) -> Node:
+        """ Функция выполняет перемещение по узлам до указанного индекса. И возвращает узел. """
+
+        if not isinstance(index, int):
+            raise TypeError()
+
+        if not 0 <= index < self._len:  # для for
+            raise IndexError()
+
+        current_node = self.head
+        for _ in range(index):
+            current_node = current_node.next
+
+        return current_node
 
     def insert(self, index: int, value: Any) -> None:
         """ Метод добавдяет узел по указанному индексу. """
